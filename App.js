cc0195d8449e53a5777765525ce99dbe5f377b8a
PEREIRA Gabriel <Gabriel.PEREIRA@cpnv.ch>
2021-05-28 12:22:37 +0200

    Create a context

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
-import 'react-native-gesture-handler';
+
+import { AuthContext } from './src/components/context'
 
 import Router from './src/Router'
 export default class App extends Component {
--- api/Api.js
+++ api/Api.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import 'react-native-gesture-handler';
 
 export default axios.create({
   baseURL: `http://localhost:8000/api/`
