8c7337e3bfade4aace5ee2c442a75a63aa442d79
PEREIRA Gabriel <Gabriel.PEREIRA@cpnv.ch>
2021-06-04 11:33:58 +0200

    update API

--- api/Api.js
+++ api/Api.js
@@ -1,6 +1,10 @@
 import axios from 'axios';
 import 'react-native-gesture-handler';
 
-export default axios.create({
-  baseURL: `http://localhost:8000/api/`
+let API = axios.create({
+  baseURL: `http://localhost:8000/api/`,
+  timeout: 10000,
 });
+
+export default API;
+export const token = localStorage.getItem('user_token')
