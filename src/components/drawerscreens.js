81dd72480584ea3331c527994e4e215639ff5937
PEREIRA Gabriel <Gabriel.PEREIRA@cpnv.ch>
2021-06-04 09:15:20 +0200

    Create other principal page

--- src/components/drawerscreens.js
+++ src/components/drawerscreens.js
@@ -5,26 +5,40 @@ import { TouchableOpacity } from 'react-native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 
-import Login from '../views/login';
+import Consult from '../views/consult';
 import PrincipalMenu from '../views/principalMenu';
+import Rapport from '../views/rapport';
 
-const LoginStack = createStackNavigator();
-const LoginStackScreen = ({ navigation }) => (
-    <LoginStack.Navigator >
-        <LoginStack.Screen name="Login" component={Login} options={{ 
-            title: "Login", 
+const ConsultStack = createStackNavigator();
+const ConsultStackScreen = ({ navigation }) => (
+    <ConsultStack.Navigator >
+        <ConsultStack.Screen name="Consulter" component={Consult} options={{ 
+            title: "Consulter", 
             headerLeft: () => (
                 <TouchableOpacity style={{ paddingLeft: 10, paddingTop: 5 }} onPress={() => navigation.openDrawer()}>
                     <Icon name='ios-menu' size={25} color='black' />
                 </TouchableOpacity>
             ) }} />
-    </LoginStack.Navigator>
+    </ConsultStack.Navigator>
+);
+
+const RapportStack = createStackNavigator();
+const RapportStackScreen = ({ navigation }) => (
+    <RapportStack.Navigator >
+        <RapportStack.Screen name="Rapporter" component={Rapport} options={{ 
+            title: "Rapporter", 
+            headerLeft: () => (
+                <TouchableOpacity style={{ paddingLeft: 10, paddingTop: 5 }} onPress={() => navigation.openDrawer()}>
+                    <Icon name='ios-menu' size={25} color='black' />
+                </TouchableOpacity>
+            ) }} />
+    </RapportStack.Navigator>
 );
 
 const PrincipalMenuStack = createStackNavigator();
 const PrincipalMenuStackScreen = ({ navigation }) => (
     <PrincipalMenuStack.Navigator >
-        <PrincipalMenuStack.Screen name="PrincipalMenu" component={PrincipalMenu} options={{ 
+        <PrincipalMenuStack.Screen name="Menu Principal" component={PrincipalMenu} options={{ 
             title: "Menu Principal", 
             headerLeft: () => (
                 <TouchableOpacity style={{ paddingLeft: 10, paddingTop: 5 }} onPress={() => navigation.openDrawer()}>
@@ -36,8 +50,9 @@ const PrincipalMenuStackScreen = ({ navigation }) => (
 
 const Drawer = createDrawerNavigator();
 export const DrawerScreen = () => (
-    <Drawer.Navigator initialRouteName="PrincipalMenu" drawerType="back" drawerContentOptions={{ labelStyle: { fontSize: 20,}, activeBackgroundColor: "rgba(160, 187, 194, 0.5)", activeTintColor: "rgb(0, 94, 146)" }} drawerStyle={{ position: "absolute", top: 60 }} >
-        <Drawer.Screen name="Login" component={LoginStackScreen} />
-        <Drawer.Screen name="PrincipalMenu" component={PrincipalMenuStackScreen} />
+    <Drawer.Navigator initialRouteName="Menu Principal" drawerType="back" drawerContentOptions={{ labelStyle: { fontSize: 20,}, activeBackgroundColor: "rgba(160, 187, 194, 0.5)", activeTintColor: "rgb(0, 94, 146)" }} drawerStyle={{ position: "absolute", top: 60 }} >
+        <Drawer.Screen name="Consulter" component={ConsultStackScreen} />
+        <Drawer.Screen name="Rapporter" component={RapportStackScreen} />
+        <Drawer.Screen name="Menu Principal" component={PrincipalMenuStackScreen} />
     </Drawer.Navigator>
 );
