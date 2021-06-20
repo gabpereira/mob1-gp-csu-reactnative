e0523bcb92d3b8f79d1bd96629c1408fce482c43
Gabriel Pereira <54536712+gabpereira@users.noreply.github.com>
2021-06-20 22:05:09 +0200

    Manage error 500

--- src/components/errorManagement.js
+++ src/components/errorManagement.js
@@ -7,7 +7,16 @@ const errorManage = (code = null) => {
 			type: 'error',
 			text1: 'Erreur 401',
 			text2: "Votre identifiant ou mot de passe n'est pas correct. \nVeuillez les vérifier avant de réessayer!",
-		}; break;
+		}; 
+		break;
+
+		case 500:
+		return {
+			type: 'error',
+			text1: 'Une erreur côté serveur est survenue.',
+			text2: "Essayer de vous déconnecter et reconnecter.\nSi l'erreur persiste contactez un administrateur pour plus d'infos."
+		};
+		break;
 
 		default:
 		return {
